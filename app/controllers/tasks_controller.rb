f2424c33b48c9f1649dca34fe078b90c1bff47f2
hiromu.chiba <fuyu.fuyu447@outlook.jp>
2020-04-20 03:18:13 +0000

    add login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,31 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in #追加
+  before_action :correct_user, only: [:destroy] #追加
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    # 変更前:@tasks = Task.order(id: :desc).page(params[:page]).per(3)
   end
   
   def show
   end 
   
   def new
-    @task = Task.new
+    @task = current_user.tasks.build  # form_with 用
+    # 変更前:@task = Task.new
   end 
   
   def create
-    @task = Task.new(task_params)
+     @task = current_user.tasks.build(task_params)
+     # 変更前:@task = Task.new(task_params)
     if @task.save
       flash[:success] = "課題が登録されました"
-      redirect_to @task
+      redirect_to root_url #@task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = "課題が登録されませんでした"
-      render :new
+      render :new  #:new
     end
   end 
   
@@ -48,8 +54,15 @@ class TasksController < ApplicationController
   def set_task
     @task = Task.find(params[:id])
   end
-  #Strong Parameter
+  
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  #下記追加項目
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to tasks_url
+    end
   end
 end
